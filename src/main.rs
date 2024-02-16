35d160faec75ae5773fc47deb104fe6b9d754cc7
Thor Kampefner <thorck@pm.me>
2024-02-15 18:34:30 -0800

    first task

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,43 @@ mod error;
 mod utils;
 
 use axum::{
+  extract::Path,
   http::StatusCode,
   response::IntoResponse,
   routing::{get, post},
   Router,
 };
 use error::MyError;
+use serde::Deserialize;
+use serde_json::from_str;
 use tracing::info;
 
 async fn hello_world() -> &'static str { "Hello, world!" }
 
 async fn error_handler() -> impl IntoResponse {
   (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct PokeWeight {
+  weight: u32,
+}
+
+/// add a GET endpoint /8/weight/<pokedex_number> that, given a pokédex number, responds with the
+/// corresponding Pokémon's weight in kilograms as a number in plain text.
+///
+/// example
+/// ```sh
+/// curl http://localhost:8000/8/weight/25
+///
+/// 6
+async fn poke_weight(Path(pokedex): Path<u32>) -> String {
+  let url = format!("https://pokeapi.co/api/v2/pokemon/{}", pokedex);
+  let res =
+    reqwest::get(url).await.expect("poke-api failed").text().await.expect("could not get text");
+  let res: PokeWeight = from_str(&res).expect("could not deserialize");
+  info!("number: {pokedex}, weight: {}", res.weight);
+  (res.weight as f32 / 10f32).to_string()
 }
 
 #[shuttle_runtime::main]
@@ -34,6 +59,7 @@ async fn main(
 
   let router = Router::new()
     .route("/", get(hello_world))
+    .route("/8/weight/:pokedex", get(poke_weight))
     .route("/-1/error", get(error_handler))
     .route("/-1/health", get(|| async { StatusCode::OK }));
 
